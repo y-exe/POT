7dbb8d0e10c5316a984f48878c530d48587aeacb
y-exe <y.exe@outlook.jp>
2025-07-09 21:41:54 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,25 @@
-const discord = require('discord.js');
+const { Events, ActivityType , MessageFlags } = require('discord.js');
 
 module.exports = {
-    name: discord.Events.ClientReady,
-    once: true, 
-    async execute(client) {
-        await client.user.setPresence({ activities: [{ name: `ぽしゃば | vps online`, type: discord.ActivityType.Custom }] });
+  name: Events.ClientReady,
+  once: true,
+  async execute(client) {
+    console.log(`${client.user.tag}としてログインしました！`);
+
+    await client.user.setPresence({
+      activities: [{
+        name: 'ぽしゃば | online (run by yexe)', 
+        type: ActivityType.Custom  
+      }],
+      status: 'DoNotDisturb'
+    });
+
+    try {
+      const data = client.commands.map(cmd => cmd.data.toJSON());
+      await client.application.commands.set(data);
+      console.log(`${data.length}個のスラッシュコマンドを登録しました。`);
+    } catch (error) {
+      console.error('コマンドの登録中にエラーが発生しました:', error);
     }
-};
+  }, 
+}; 
